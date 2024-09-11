10628a82a9e8a191b894fe9ab0cb96c35e992087
doyeun93 <doyeun93@naver.com>
2024-09-11 13:53:48 +0900

    s

--- nextjs-wiki-book/.storybook/preview.ts
+++ nextjs-wiki-book/.storybook/preview.ts
@@ -1,14 +1,68 @@
-import type { Preview } from "@storybook/react";
+// import type { Preview } from "@storybook/react";
 
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+// const preview: Preview = {
+//   parameters: {
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/i,
+//       },
+//     },
+//   },
+// };
+
+// export default preview;
+
+// import  { addDecorator} from "@storybook/react";
+import {createGlobalStyle, ThemeProvider} from 'styled-components'
+import {theme} from "../src/themes"
+import * as NextImage from 'next/image'
+
+
+export const parameters = {
+  actions: {argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
-};
+}
 
-export default preview;
+export const GlobalStyle = createGlobalStyle`
+html,
+body,
+textarea {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+* {
+  box-sizing: border-box;
+}
+a{
+  text-decoration : none;
+  transition : .25s;
+  color: #000000;
+}
+`
+
+// addDecorator((story) => (
+//   <ThemeProvider theme={theme}>
+//   <GlobalStyle />
+//   {story()}
+//   </ThemeProvider>
+// ))
+
+
+const OriginalNextImage = NextImage.default;
+
+// Object.defineProperty(NextImage, 'default', {
+//   configurable : true,
+//   value: (props) => typeof props.src === 'string' ? (
+//     <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+//   ) : (
+//     <OriginalNextImage {...props} unoptimized />
+//   ),
+// })
